4c6b2678da3c4706f67c405399d73f15aa6e68a0
Naveen <naveengummella.rg@gmail.com>
2024-03-17 18:30:44 +0530

    yaaaaaaaaaaaaaaaar

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,5 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config");
+import { getDefaultConfig } from "expo/metro-config";
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
-  // [Web-only]: Enables CSS support in Metro.
-  isCSSEnabled: true,
-});
+const config = getDefaultConfig(__dirname);
 
 module.exports = config;
