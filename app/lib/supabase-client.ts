d46af0534c0bc23001c64a1516e3979132d1df42
Naveen <naveengummella.rg@gmail.com>
2024-03-27 00:36:52 +0530

    harcoding the api keys to test

--- app/lib/supabase-client.ts
+++ app/lib/supabase-client.ts
@@ -20,8 +20,9 @@ const ExpoSecureStoreAdapter: StorageAdapter = {
   },
 };
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY as string;
+const supabaseUrl = "https://hnmqvjnnscdkcbxcbvyu.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhubXF2am5uc2Nka2NieGNidnl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyOTgxMjMsImV4cCI6MjAyNjg3NDEyM30.xjzSaG22KLkzskY4U3NaohE_RLTLTkeTW_2qD5RFPUc";
 
 export const supabase: SupabaseClient = createClient(
   supabaseUrl,
