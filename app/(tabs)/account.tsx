05da13e318e2d06c1a50dd8382e31a524fe401e2
Naveen <naveengummella.rg@gmail.com>
2024-03-29 21:52:41 +0530

    nice

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -13,8 +13,6 @@ import AccountSheet from "../../components/bottomSheet";
 
 import { supabase } from "@/app/lib/supabase-client";
 
-//TODO: add a notification icon
-//TODO: add a profile icon
 //TODO: details grids of profile picture, name, email, phone number, address, and edit button
 //TODO: details of flat no, family members, parking, your posts, your events, your polls, your announcements, your notices, your chats, your complaints, your suggestions, your feedbacks, your maintenance, your bills, your payments, your receipts, your orders,
 //TODO: current and past utilities, current and past maintenance, current and past bills, current and past orders, current and past payments, current and past receipts, current and past complaints, current and past suggestions, current and past feedbacks, current and past polls, current and past announcements, current and past notices, current and past chats, current and past events, current and past posts, current and past family members, current and past parking, current and past flat no, current and past name, current and past email, current and past phone number, current and past address, current and past profile picture, current and past edit button
